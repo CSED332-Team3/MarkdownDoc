bc2c69d4d6cf19e55207e5fdfe198306435058c1
Taegyu Park <computerpark@postech.ac.kr>
2020-11-13 01:44:01 +0900

    Setup for testing environment
    !! EXPERIMENTAL !!
    Currently tuning ongoing.

--- src/test/java/edu/postech/csed332/team3/MarkdownDoc/JxBrowserTest.java
+++ src/test/java/edu/postech/csed332/team3/MarkdownDoc/JxBrowserTest.java
@@ -1,4 +1,12 @@
 package edu.postech.csed332.team3.MarkdownDoc;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class JxBrowserTest {
+    @Test
+    public void smokeTest() {
+        assertEquals(3, 1 + 2);
+    }
 }
