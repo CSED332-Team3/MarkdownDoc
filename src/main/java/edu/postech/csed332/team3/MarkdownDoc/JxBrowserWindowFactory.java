c32ee578285e4520ed6c651465ab66b097f9501f
Joseph Yoon <joseph@jyoon.pro>
2020-11-12 23:54:28 +0900

    Setup base project structure
    - Add packages
    - Update gradle dependencies
    - Add .gitlab-ci.yml template
    - Configure sidebar in plugin.xml
    - Configure gradle tasks

---
+++ src/main/java/edu/postech/csed332/team3/MarkdownDoc/JxBrowserWindowFactory.java
@@ -0,0 +1,13 @@
+package edu.postech.csed332.team3.MarkdownDoc;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class JxBrowserWindowFactory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+    }
+}
