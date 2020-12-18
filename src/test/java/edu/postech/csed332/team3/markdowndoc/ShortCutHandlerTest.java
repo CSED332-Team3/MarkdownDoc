7e0781592ff75ef647c61a7a3241dcdaec48a150
Moon saemi <saemi@postech.ac.kr>
2020-12-18 20:39:51 +0900

    Refactoring ShortCutAction and ShortCutHandler

--- src/test/java/edu/postech/csed332/team3/markdowndoc/ShortCutHandlerTest.java
+++ src/test/java/edu/postech/csed332/team3/markdowndoc/ShortCutHandlerTest.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 import static java.awt.event.InputEvent.CTRL_DOWN_MASK;
 import static java.awt.event.KeyEvent.VK_SEMICOLON;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 public class ShortCutHandlerTest {
     @Test
@@ -23,22 +22,5 @@ public class ShortCutHandlerTest {
         assertEquals(handler.getShortcut(), new KeyboardShortcut(handler.getKeyStroke(), null));
     }
 
-    @Test
-    public void TestGetKeyCode() {
-        ShortCutHandler handler = new ShortCutHandler(VK_SEMICOLON, CTRL_DOWN_MASK, false);
-        assertEquals(VK_SEMICOLON, handler.getKeyCode());
-    }
-
-    @Test
-    public void TestGetModifiers() {
-        ShortCutHandler handler = new ShortCutHandler(VK_SEMICOLON, CTRL_DOWN_MASK, false);
-        assertEquals(CTRL_DOWN_MASK, handler.getModifiers());
-    }
-
-    @Test
-    public void TestGetOnKeyRelease() {
-        ShortCutHandler handler = new ShortCutHandler(VK_SEMICOLON, CTRL_DOWN_MASK, false);
-        assertFalse(handler.getOnKeyRelease());
-    }
 }
 
