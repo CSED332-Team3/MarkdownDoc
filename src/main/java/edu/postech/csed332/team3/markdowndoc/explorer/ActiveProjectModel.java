a9ec5c28e84a888d987f90ea9a30caed088b15bc
kdh0324 <kdh0324@postech.ac.kr>
2020-12-27 23:15:07 +0900

    Extract method about getting active project directory

--- src/main/java/edu/postech/csed332/team3/markdowndoc/explorer/ActiveProjectModel.java
+++ src/main/java/edu/postech/csed332/team3/markdowndoc/explorer/ActiveProjectModel.java
@@ -1,7 +1,11 @@
 package edu.postech.csed332.team3.markdowndoc.explorer;
 
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.ex.temp.TempFileSystem;
 import com.intellij.openapi.wm.WindowManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,5 +30,15 @@ public class ActiveProjectModel {
         }
         // if there is no active project, return an arbitrary project (the first)
         return ProjectManager.getInstance().getOpenProjects()[0];
+    }
+
+    public static String getProjectDir() {
+        VirtualFile projectRoot = ModuleRootManager.getInstance(
+                ModuleManager.getInstance(getActiveProject()).getModules()[0]
+        ).getContentRoots()[0];
+        String path = projectRoot.getCanonicalPath();
+        if (projectRoot.getFileSystem() instanceof TempFileSystem && path != null && path.startsWith("/"))
+            path = path.replaceFirst("/", "");
+        return path;
     }
 }
